d0940c56f92d6e1b8c75df3407e512bca7d31ded
Precious <preciousmaregwa1997@gmail.com>
2022-06-11 07:33:37 +0300

    complete assigment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves namesArray # code an argument here
   # Your code here
+  namesArray.each.with_index { |name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet planeteers # code an argument here
   # Your code here
+  planeteers.map { |planet| "#{planet.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls planeteers # code an argument here
   # Your code here
+  planeteers.each { |planet| 
+  if planet.size > 4
+    return true
+  end
+ }
+ return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese string_array # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if string_array.include? cheese
+      return cheese
+    else
+      return nil
+    end
+  end
 end
